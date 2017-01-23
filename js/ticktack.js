1b2e013803cbc33868870f1e773d07ab27aba68c
Louis the Noob <dev.louis.paul@outlook.de>
2017-01-23 18:15:32 +0100

    Update ticktack.js

--- js/ticktack.js
+++ js/ticktack.js
@@ -4,38 +4,35 @@
 		var todayDate = new Date(),
 			// call all the things the clock needs
 			// Year / Month / Date/Day
-			
-			var year = todayDate.getFullYear(),
-			var month = todayDate.getMonth(),
-			var date = todayDate.getDate(),
-			var day = todayDate.getDay(),
-			
+			year = todayDate.getFullYear(),
+			month = todayDate.getMonth(),
+			date = todayDate.getDate(),
+			day = todayDate.getDay(),
 			// Hours / Minutes / Seconds
-			var h = todayDate.getHours(),
-			var m = todayDate.getMinutes(),
-			var s = todayDate.getSeconds();
-		
+			h = todayDate.getHours(),
+			m = todayDate.getMinutes(),
+			s = todayDate.getSeconds();
 		// what goes where?
 		var zYear = document.getElementById("year"),
-			var zMonth = document.getElementById("month"),
-			var zDate = document.getElementById("date"),
-			var zDay = document.getElementById("day"),
-			var zHour = document.getElementById("hours"),
-			var zMin = document.getElementById("minutes"),
-			var zSec = document.getElementById("seconds");
-		
-		
-		zYear.textContent = year;
-		
-		// Array for all the months
-		var months = ["January","Febuary","March","April","May","June","July","August","September","October","November","December"]
-		zMonth.textContent = months[month];
-		
-		zDate.textContent = date;
+			zMonth = document.getElementById("month"),
+			zDate = document.getElementById("datey"), //funfact...  "date" wont work... took me a lot of time to figure this out 
+			zDay = document.getElementById("day"),
+			zHour = document.getElementById("hours"),
+			zMin = document.getElementById("minutes"),
+			zSec = document.getElementById("seconds");
 		
 		// Array for all of the Weekdays
 		var weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-		zDay = weekDays[day];
+		zDay.textContent = weekDays[day];
+		
+		zDate.textContent = date;
+		
+		var months = ["January","Febuary","March","April","May","June","July","August","September","October","November","December"]
+		zMonth.textContent = months[month];
+		
+		zYear.textContent = year;
+		
+		
 		
 		zHour.textContent = h;
 		zMin.textContent = m;
@@ -43,6 +40,5 @@
 	};
 
 	refresher();
-	var interval = setInterval(refresher,1000);
-	alert("Hello! I am an alert box!");
+	var dieter = setInterval(refresher, 1000);
 }())
